ab9a8d6003db090d269a9768fea477d713ad0f9a
yashanand167 <yash.anand167@gmail.com>
2026-07-26 19:51:21 +0530

    feat: overhaul home page with custom header and initial experience section components

---
+++ components/header.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <div className="flex flex-row gap-2 items-center justify-center">
+        <div className="aspect-square bg-white border border-zinc-300 rounded-2xl">
+            <Image src="/Logo.png" alt="logo" width={50} height={50}/>
+        </div>
+      <ul className="flex flex-row gap-4 text-base font-medium">
+        <li>Home</li>
+        <li>Work</li>
+        <li>Blogs</li>
+      </ul>
+    </div>
+  );
+}
